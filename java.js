2518c44270043ed4b59ff5e2579e72c237644956
galatisr <98347972+galatisr@users.noreply.github.com>
2022-12-05 21:56:15 -0500

    new java

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
 window.onload = setTimeout(function(){
     alert('Sorry, it seems we found another candidate who was better fit for the job. We encourage you to apply for another position.');
+    window.location = 'https://galatisr.github.io/index.html'
 }, 20000);
