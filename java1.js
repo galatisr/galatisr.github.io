71f73be087efd513e7f18f0caecbf496af32350f
galatisr <98347972+galatisr@users.noreply.github.com>
2022-12-06 14:43:13 -0500

    newest code

--- java1.js
+++ java1.js
@@ -1,4 +1,6 @@
 window.onload = setTimeout(function(){
-    alert("Welcome to the team! You're hired! Your first shift starts tomorrow at 4:00AM :)");
-      window.location = 'https://galatisr.github.io/index.html'
+    alert("Thanks for choosing to work local! Welcome to the team, You're hired! Your first shift starts tomorrow at 4:00AM :)");
 }, 20000);
+
+var answer = alert ("Thanks for choosing to work local! Welcome to the team, You're hired! Your first shift starts tomorrow at 4:00AM :)")
+window.location="https://forms.gle/EXH6P97R4XkVNvm4A"
