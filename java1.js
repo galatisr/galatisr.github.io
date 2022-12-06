b33a08b37c73b4b66d371b8728401a08d6e5d63e
galatisr <98347972+galatisr@users.noreply.github.com>
2022-12-05 21:56:57 -0500

    newer java

--- java1.js
+++ java1.js
@@ -1,3 +1,4 @@
 window.onload = setTimeout(function(){
     alert("Welcome to the team! You're hired! Your first shift starts tomorrow at 4:00AM :)");
+      window.location = 'https://galatisr.github.io/index.html'
 }, 20000);
